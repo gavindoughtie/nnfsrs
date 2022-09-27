fe459ede03f7c0aef22282d5a3b3fd0b05367275
gavin.doughtie <gavin.doughtie@gmail.com>
2022-09-27 08:03:11 -0700

    F! Implemented a basic neuron

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // Basic neuron with hard-coded values
+    let inputs = [1.2, 5.1, 2.1];
+    let weights = [3.1, 2.1, 8.7];
+    let bias = 3.0;
+    let output = 
+        inputs[0] * weights[0] +
+        inputs[1] * weights[1] +
+        inputs[2] * weights[2] +
+        bias;
+    println!("output: {}", output);
 }
